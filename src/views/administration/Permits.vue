e76c28fbae1a07e497470baafdf163a493957344
mikehdz743@gmail.com <mikehdz743@gmail.com>
2021-01-31 01:08:18 -0600

    Permits

--- src/views/administration/Permits.vue
+++ src/views/administration/Permits.vue
@@ -1,10 +1,140 @@
 <template>
   <div>
-    <v-chip class="mb-5" color="secondary" label text-color="white">
-      Permisos
-      <v-icon right>
-        mdi-text-box-plus
-      </v-icon>
-    </v-chip>
+    <v-row class="my-5 mx-1">
+      <v-chip class="mb-5" color="secondary" label text-color="white">
+        <h3>
+          Permisos
+        </h3>
+        <v-icon right>
+          mdi-text-box-plus
+        </v-icon>
+      </v-chip>
+    </v-row>
+    <!-- START CONTENT -->
+    <v-row>
+      <v-col cols="8" offset="2">
+        <template>
+          <v-expansion-panels>
+            <v-expansion-panel
+              v-for="(item, index) in listPermits"
+              :key="index"
+            >
+              <v-expansion-panel-header>
+                {{ item.username }}
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <!-- Listado Completo -->
+                <v-switch
+                  label="Listado Completo"
+                  :input-value="item.listado_completo"
+                  @change="(value) => changeState(value, index, 1)"
+                ></v-switch>
+                <!-- Panel de administracion -->
+                <v-switch
+                  label="Panel de administracion"
+                  :input-value="item.panel_administrativo"
+                  @change="(value) => changeState(value, index, 2)"
+                ></v-switch>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </template>
+      </v-col>
+    </v-row>
+    <!-- START SNACKBAR -->
+    <v-snackbar
+      v-model="snackbar.state"
+      :timeout="3000"
+      :color="snackbar.color"
+      left
+    >
+      {{ snackbar.message }}
+      <v-btn text v-bind="attrs" @click="snackbar.state = false">
+        Cerrar
+      </v-btn>
+    </v-snackbar>
+    <!-- END SNACBAR -->
   </div>
 </template>
+<script>
+export default {
+  name: "Permits",
+  data() {
+    return {
+      listPermits: [
+        {
+          id: 1,
+          username: "Carol Sholl",
+          listado_completo: true,
+          panel_administrativo: true,
+        },
+        {
+          id: 2,
+          username: "Eydie Skully",
+          listado_completo: false,
+          panel_administrativo: true,
+        },
+        {
+          id: 3,
+          username: "Gabriello Gillbee",
+          listado_completo: false,
+          panel_administrativo: false,
+        },
+        {
+          id: 4,
+          username: "Raye Morgan",
+          listado_completo: false,
+          panel_administrativo: false,
+        },
+        {
+          id: 5,
+          username: "Valery Monday",
+          listado_completo: true,
+          panel_administrativo: false,
+        },
+      ],
+      //new permits:
+      permits: {},
+      //Dialog
+      dialog: false,
+      name: "",
+      state: false,
+      /* Snackbar */
+      snackbar: {
+        state: false,
+        message: "",
+        color: "",
+      },
+      attrs: {},
+    };
+  },
+  methods: {
+    changeState(value, index, opcion) {
+      switch (opcion) {
+        case 1:
+          this.listPermits[index].listado_completo = value;
+          this.showSnackbar(
+            "Permiso Actualizado - Listados Completos",
+            "success"
+          );
+          break;
+        case 2:
+          this.listPermits[index].panel_administrativo = value;
+          this.showSnackbar(
+            "Permiso Actualizado - Panel Administrativo",
+            "success"
+          );
+          break;
+
+        default:
+          break;
+      }
+    },
+    showSnackbar(message, color) {
+      this.snackbar.message = message;
+      this.snackbar.color = color;
+      this.snackbar.state = true;
+    },
+  },
+};
+</script>
--- src/views/administration/VisitingState.vue
+++ src/views/administration/VisitingState.vue
@@ -6,5 +6,285 @@
         mdi-text-box-plus
       </v-icon>
     </v-chip>
+    <!-- START TABLE -->
+    <template>
+      <v-simple-table>
+        <template>
+          <thead>
+            <tr>
+              <th class="text-left">
+                No.
+              </th>
+              <th class="text-left">
+                Nombre
+              </th>
+              <th class="text-left">
+                Color
+              </th>
+              <th class="text-left">
+                Visible
+              </th>
+              <th class="text-left">
+                Acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in listClassification" :key="item.nombre">
+              <td>{{ index + 1 }}</td>
+
+              <td>{{ item.nombre }}</td>
+              <td>
+                <v-chip
+                  class="ma-2"
+                  :color="item.material_color"
+                  text-color="white"
+                >
+                  {{ item.nombre }}
+                </v-chip>
+              </td>
+              <td>
+                <v-switch
+                  :input-value="item.visible"
+                  @change="(value) => changeState(value, index)"
+                ></v-switch>
+              </td>
+              <td>
+                <!-- BTN Edit -->
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon @click="edit(index)">
+                      <v-icon dark v-bind="attrs" v-on="on">
+                        mdi-pencil
+                      </v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Editar</span>
+                </v-tooltip>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </template>
+    <!-- END TABLE -->
+    <!-- START MODAL CREATE -->
+    <v-dialog v-model="dialog" persistent max-width="600">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Crear Clasificacion</span>
+        </v-card-title>
+        <v-form @submit.prevent="createClassification">
+          <v-card-text>
+            <!-- INPUT -->
+            <v-text-field
+              outlined
+              v-model="nombre"
+              label="Nombre *"
+              name="nombre"
+              autofocus
+              required
+            ></v-text-field>
+
+            <!-- COMPONENT  -->
+
+            <!-- COMPONENT -->
+
+            <small>* indica un campo obligatorio</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Cancelar
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              type="submit"
+              @click="dialog = false"
+            >
+              Guardar
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-dialog>
+    <!-- END MODAL CREATE -->
+
+    <!-- START MODAL EDIT -->
+    <v-dialog v-model="dialogEdit" persistent max-width="600">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Editar Clasificacion</span>
+        </v-card-title>
+        <v-form @submit.prevent="editClassification">
+          <v-card-text>
+            <!-- INPUT -->
+            <v-text-field
+              outlined
+              v-model="nombre"
+              label="Nombre *"
+              name="nombre"
+              autofocus
+              required
+            ></v-text-field>
+
+            <!-- COMPONENT  -->
+
+            <!-- COMPONENT -->
+
+            <small>* indica un campo obligatorio</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="dialogEdit = false">
+              Cancelar
+            </v-btn>
+            <v-btn
+              color="green darken-1"
+              text
+              type="submit"
+              @click="dialogEdit = false"
+            >
+              Editar
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-dialog>
+    <!-- END MODAL EDIT -->
+
+    <!-- START SNACKBAR -->
+    <v-snackbar
+      v-model="snackbar.state"
+      :timeout="3000"
+      :color="snackbar.color"
+      left
+    >
+      {{ snackbar.message }}
+      <v-btn text v-bind="attrs" @click="snackbar.state = false">
+        Cerrar
+      </v-btn>
+    </v-snackbar>
+    <!-- END SNACBAR -->
   </div>
 </template>
+
+<script>
+export default {
+  name: "Classification",
+  data() {
+    return {
+      listClassification: [
+        {
+          id: 1,
+          nombre: "Fisica",
+          material_color: "primary",
+          visible: true,
+        },
+        {
+          id: 2,
+          nombre: "Geofisica",
+          material_color: "warning",
+          visible: true,
+        },
+        {
+          id: 3,
+          nombre: "Geologia",
+          material_color: "pink",
+          visible: false,
+        },
+      ],
+      dialog: false,
+      dialogEdit: false,
+
+      nombre: "",
+      color: null,
+      indexClassification: null,
+      idClassification: null,
+      /* Snackbar */
+      snackbar: {
+        state: false,
+        message: "",
+        color: "",
+      },
+      /* Tooltip */
+      on: true,
+      attrs: {},
+    };
+  },
+  methods: {
+    create() {
+      this.settingdata();
+      this.dialog = true;
+    },
+    changeState(valor, index) {
+      this.listClassification[index].visible = valor;
+      this.showSnackbar("Clasificacion Actualizada", "success");
+    },
+    createClassification() {
+      //Default Color
+      this.color = "secondary";
+
+      if (this.validate(this.nombre, this.color)) {
+        this.listClassification.push({
+          id: Date.now(),
+          nombre: this.nombre,
+          material_color: this.color,
+          visible: true,
+        });
+        this.showSnackbar("Clasificación Agregado", "primary");
+      }
+      this.settingdata();
+    },
+    edit(index) {
+      this.nombre = this.listClassification[index].nombre;
+      this.color = this.listClassification[index].material_color;
+      this.indexClassification = index;
+      this.dialogEdit = true;
+    },
+    editClassification() {
+      if (this.validate(this.nombre, this.color)) {
+        this.listClassification[this.indexClassification].nombre = this.nombre;
+        this.listClassification[
+          this.indexClassification
+        ].material_color = this.color;
+        this.showSnackbar("Clasificacion Editado", "success");
+        this.settingdata();
+      }
+    },
+
+    showSnackbar(message, color) {
+      this.snackbar.message = message;
+      this.snackbar.color = color;
+      this.snackbar.state = true;
+    },
+    settingdata() {
+      this.nombre = "";
+      this.color = null;
+      this.indexClassification = null;
+      this.idClassification = null;
+    },
+    validate(nombre, color) {
+      if (color === null && nombre.length == 0) {
+        this.showSnackbar(
+          "Los campos de nombre y color estan vacios",
+          "red"
+        );
+        return false;
+      } else if (nombre.length === 0) {
+        this.showSnackbar(
+          "El campo del nombre de la Clasificación esta vacio",
+          "red"
+        );
+        return false;
+      } else if (color === null) {
+        this.showSnackbar("El color esta vacio", "red");
+        return false;
+      }
+      return true;
+    },
+  },
+};
+</script>
+
