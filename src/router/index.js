106b820ae4132e7d0e705ca96fa059de6091a196
kernel503 <juanko682@gmail.com>
2021-02-21 15:11:03 -0600

    hash router

--- src/router/index.js
+++ src/router/index.js
@@ -59,7 +59,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   // base: process.env.BASE_URL || '/geocimat',
   base: process.env.BASE_URL + '/geocimat',
   routes,
