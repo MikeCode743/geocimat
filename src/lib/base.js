ccff3dde74d078ed6f51bd5b87409a3f9452652e
kernel503 <juanko682@gmail.com>
2021-02-22 00:55:28 -0600

    token

--- src/lib/base.js
+++ src/lib/base.js
@@ -1,7 +1,7 @@
 // xcopy /e /y dist D:\Programas\laragon\www\geocimat-backend\public && xcopy /y dist\index.html D:\Programas\laragon\www\geocimat-backend\resources\views\geocimat\index.blade.php
 // document.querySelector('[name="_token"]')?.value;
 const axios = require('axios');
-const config = { 'X-CSRF-TOKEN': window.Laravel.token, otro: 'header' };
+const config = { 'X-CSRF-TOKEN': window.Laravel.token };
 const baseURL = '';
 // const baseURL = 'http://127.0.0.1:8000';
 // const baseURL = 'https://geocimat.herokuapp.com';
